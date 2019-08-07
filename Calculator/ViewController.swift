90602a1b6ce926ae538f15b7da8133e7543c7809
Mustafa Kabaktepe <m.kabaktepe@outlook.com>
2019-08-08 01:39:47 +0200

    initial commit

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,22 +10,72 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let numbersArray = [0,1,2,3,4,5,6,7,8,9]
+    
     @IBOutlet weak var displayLabel: UILabel!
     
-    
+    private var isFinishedTypingNumber = true
     
     @IBAction func calcButtonPressed(_ sender: UIButton) {
         
         //What should happen when a non-number button is pressed
-    
+        print("non-number button is pressed")
+        isFinishedTypingNumber = true
+        
+        guard let number = Double(displayLabel.text!) else {fatalError("Cannot convert displaylabel text to double")}
+        
+        if let tagValue = sender.currentTitle {
+            switch(tagValue) {
+            case "AC":
+                displayLabel.text = String(Int(number * 0))
+            case "+/-":
+                displayLabel.text = String(number * -1)
+            case "%":
+                displayLabel.text = String(number/100)
+            case "÷":
+                print("÷")
+            case "x":
+                print("x")
+                
+            case "-":
+                print("-")
+                
+            case "+":
+                print("+")
+                
+            case "=":
+                print("=")
+            default:
+                print("default")
+            }
+        }
     }
-
+    
     
     @IBAction func numButtonPressed(_ sender: UIButton) {
         
         //What should happen when a number is entered into the keypad
-    
-    }
+        if let numValue = sender.currentTitle {
+            
+            if isFinishedTypingNumber {
+                displayLabel.text = numValue
+                isFinishedTypingNumber = false
+            }
+            else {
+                if numValue == "." {
+                    guard let number = Double(displayLabel.text!) else {fatalError("Cannot convert displaylabel text to double")}
 
+                    let isInt = floor(number) == number
+                    
+                    if !isInt {
+                        return
+                    }
+                }
+                displayLabel.text = displayLabel.text! + numValue
+            }
+            
+        }
+    }
+    
 }
 
